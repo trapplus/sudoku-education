f6779123df0f1639c58587b911ba8dc1ef992a75
trapplus <blazer19092008@gmail.com>
2026-01-17 16:19:26 +0300

    remove validation func from engine

--- include/sudoku-education/Engine.h
+++ include/sudoku-education/Engine.h
@@ -8,9 +8,4 @@ public:
     bool isMoveValid(const Board& board, int row, int col, int value) const;
     void applyMove(Board& board, int row, int col, int value);
     bool solve(Board& board);
-
-private:
-    bool isRowValid(const Board& board, int row, int value) const;
-    bool isColValid(const Board& board, int col, int value) const;
-    bool isBoxValid(const Board& board, int row, int col, int value) const;
 };
---
+++ include/sudoku-education/cli.h
@@ -0,0 +1,6 @@
+class cli {
+    public:
+    
+    private:
+    
+};
